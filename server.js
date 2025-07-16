c821e3073ce4a83f1d57c4842d56f6cea00b10c0
fatimahz-ga <fatimaabdullhafeez@gmail.com>
2025-07-16 10:58:53 +0300

    last

--- server.js
+++ server.js
@@ -1,40 +1,47 @@
-const dotenv = require("dotenv");
-dotenv.config();
-const express = require("express");
-const app = express();
+require('dotenv').config({ quiet: true })
+const express = require('express')
+const app = express()
+const methodOverride = require('method-override')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const authController = require('./controllers/auth.controller')
+const isSignedIn = require('./middleware/is-signed-in')
+const passUserToView = require('./middleware/pass-user-to-view')
 
-const mongoose = require("mongoose");
-const methodOverride = require("method-override");
-const morgan = require("morgan");
+// DATABASE CONNECTION
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name} 🙃.`)
+})
 
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+app.use(morgan('dev'))
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI,
+    })
+}))
+app.use(passUserToView)
 
-const authController = require("./controllers/auth.js");
+app.get('/', (req, res) => {
+    res.render('index.ejs', { title: 'my App'})
+})
 
+// ROUTES
+app.use('/auth', authController)
 
-// Router
-app.get("/", async (req, res) => {
-  res.render("index.ejs");
-});
+app.get('/vip-lounge', isSignedIn, (req, res) => {
+    res.send(`Welcome ✨`)
+})
 
-
-
-// Set the port from environment variable or default to 3000
-const port = process.env.PORT ? process.env.PORT : "3000";
-
-mongoose.connect(process.env.MONGODB_URI);
-
-mongoose.connection.on("connected", () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
-
-// Middleware to parse URL-encoded data from forms
-app.use(express.urlencoded({ extended: false }));
-// Middleware for using HTTP verbs such as PUT or DELETE
-app.use(methodOverride("_method"));
-// Morgan for logging HTTP requests
-app.use(morgan('dev'));
-app.use("/auth", authController);
-
+const port = process.env.PORT ? process.env.PORT : "3000"
 app.listen(port, () => {
-  console.log(`The express app is ready on port ${port}!`);
-});
+    console.log(`The express app is ready on port ${port}`)
+})
